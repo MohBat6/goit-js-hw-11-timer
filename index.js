1f9a1a4b4190eee79d10b30b292e6454445e049d
Evgeniy Yurchak <zhenyaa2007@gmail.com>
2021-06-06 20:40:27 +0300

    edited

--- index.js
+++ index.js
@@ -1,50 +1,50 @@
-const refs = {
-    daysCount: document.querySelector('.value[data-value="days"]'),
-    hoursCount: document.querySelector('.value[data-value="hours"]'),
-    minsCount: document.querySelector('.value[data-value="mins"]'),
-    secsCount: document.querySelector('.value[data-value="secs"]'),
-  }
-  
-  class CountdownTimer {
-    timeComponents = { days: 0, hours: 0, mins: 0, secs: 0 };
-  
-    constructor({targetDate}) {
+class CountdownTimer {
+  constructor({targetDate}) {
+      
+      this.days= document.querySelector('[data-value="days"]');
+      this.hours= document.querySelector('[data-value="hours"]');
+      this.mins= document.querySelector('[data-value="mins"]');
+      this.secs = document.querySelector('[data-value="secs"]');
       this.targetDate = targetDate;
-    }
-  
-    count() {
-      const startTime = this.targetDate;
-      setInterval(() => {
-        const currentTime = Date.now();
-        const deltaTime = startTime - currentTime;
-        this.getTimeComponents(deltaTime);
-        this.updCounter() 
-      }, 1000)
-    }
-  
+      this.intervalId = null;
+  };
+
+  start() {
+     this.intervalId= setInterval(() => {
+          const deltaTime = this.targetDate - Date.now();
+          const time = this.getTimerComponents(deltaTime);
+         this.updateClockface(time);
+      }, 1000);
+  };
+
+
     pad(value) {
-      return String(value).padStart(2, '0');
-    }
+  return String(value).padStart(2, '0');
+  };
   
-    getTimeComponents(time) {
-      this.timeComponents.days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-      this.timeComponents.hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-      this.timeComponents.mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-      this.timeComponents.secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
   
-    }
-  
-    updCounter() {
-      refs.daysCount.textContent = this.timeComponents.days;
-      refs.hoursCount.textContent = this.timeComponents.hours;
-      refs.minsCount.textContent = this.timeComponents.mins;
-      refs.secsCount.textContent = this.timeComponents.secs;
-    }
+      getTimerComponents(time) {
+  const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+  const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+  const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+  const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+  return { days, hours, mins, secs };
   }
-  
-  const timer = new CountdownTimer({
-    selector: '#timer-1',
-    targetDate: new Date('Jun 01, 2021'),
-  });
-  
-  timer.count()
+
+  updateClockface({days, hours, mins, secs}) {
+this.days.textContent = `${days}`;
+this.hours.textContent = `${hours}`;
+this.mins.textContent = `${mins}`;
+this.secs.textContent = `${secs}`;
+  };
+
+}
+
+const timer = new CountdownTimer({
+  selector: '#timer-1',
+targetDate: new Date('Jul 17, 2021'),
+});
+
+
+timer.start();
